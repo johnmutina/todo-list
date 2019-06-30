68b6ca65133f8d6201cc64a27a14c17deaddc96f
johnmutina <johnmutina@gmail.com>
2019-06-30 18:51:29 +0200

    Change app name

--- src/Todo.js
+++ src/Todo.js
@@ -2,31 +2,31 @@ import React, { Component } from "react";
 import "./Todo.css";
 
 class Todo extends Component {
-  constructor(props) {
-    super(props);
-    this.handleClick = this.handleClick.bind(this);
-    this.handleEdit = this.handleEdit.bind(this);
-  }
+    constructor(props) {
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+        this.handleEdit = this.handleEdit.bind(this);
+    }
 
-  handleEdit() {
-    this.props.editTodo(this.props.id);
-  }
+    handleEdit() {
+        this.props.editTodo(this.props.id);
+    }
 
-  handleClick() {
-    this.props.removeTodo(this.props.id);
-  }
+    handleClick() {
+        this.props.removeTodo(this.props.id);
+    }
 
-  render() {
-    return (
-      <div className="Todo">
-        <h5>{this.props.text}</h5>
-        <div>
-          <button onClick={this.handleEdit}>Edit</button>
-          <button onClick={this.handleClick}>X</button>
-        </div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="Todo List">
+                <h5>{this.props.text}</h5>
+                <div>
+                    <button onClick={this.handleEdit}>Edit</button>
+                    <button onClick={this.handleClick}>X</button>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Todo;
