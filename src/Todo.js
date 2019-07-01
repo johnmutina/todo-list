a2a5a2d57732ac58e86b8d92ddda6b1c63650452
johnmutina <johnmutina@gmail.com>
2019-07-01 22:39:17 +0200

    Update edit functionality

--- src/Todo.js
+++ src/Todo.js
@@ -1,31 +1,72 @@
 import React, { Component } from "react";
 import "./Todo.css";
+import { throwStatement } from "@babel/types";
 
 class Todo extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            isEditing: false,
+            text: this.props.text
+        };
         this.handleClick = this.handleClick.bind(this);
         this.handleEdit = this.handleEdit.bind(this);
+        this.handleUpdate = this.handleUpdate.bind(this);
+        this.handleChange = this.handleChange.bind(this);
     }
 
     handleEdit() {
-        this.props.editTodo(this.props.id);
+        this.setState({
+            isEditing: !this.state.isEditing
+        });
     }
 
     handleClick() {
         this.props.removeTodo(this.props.id);
     }
 
+    handleUpdate(evt) {
+        evt.preventDefault();
+        this.props.editTodo(this.props.id, this.state.text);
+        this.setState({
+            isEditing: false
+        });
+    }
+
+    handleChange(evt) {
+        this.setState({
+            [evt.target.name]: evt.target.value
+        });
+    }
+
     render() {
-        return (
-            <div className="Todo List">
-                <h5>{this.props.text}</h5>
-                <div>
-                    <button onClick={this.handleEdit}>Edit</button>
-                    <button onClick={this.handleClick}>X</button>
+        let result;
+        if (this.state.isEditing) {
+            result = (
+                <div className="Todo List">
+                    <form onSubmit={this.handleUpdate}>
+                        <input
+                            name="text"
+                            id="text"
+                            value={this.state.text}
+                            onChange={this.handleChange}
+                        />
+                        <button>Save</button>
+                    </form>
                 </div>
-            </div>
-        );
+            );
+        } else {
+            result = (
+                <div className="Todo List">
+                    <h5>{this.props.text}</h5>
+                    <div>
+                        <button onClick={this.handleEdit}>Edit</button>
+                        <button onClick={this.handleClick}>X</button>
+                    </div>
+                </div>
+            );
+        }
+        return result;
     }
 }
 
--- src/TodoList.js
+++ src/TodoList.js
@@ -4,56 +4,68 @@ import uuid from "uuid";
 import Todo from "./Todo";
 
 class TodoList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      todos: []
-    };
-    this.createNewTodo = this.createNewTodo.bind(this);
-    this.removeTodo = this.removeTodo.bind(this);
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            todos: []
+        };
+        this.createNewTodo = this.createNewTodo.bind(this);
+        this.removeTodo = this.removeTodo.bind(this);
+        this.updateText = this.updateText.bind(this);
+    }
 
-  createNewTodo(todoObj) {
-    let uniqueId = uuid();
-    let newTodo = {
-      ...todoObj,
-      id: uniqueId,
-      key: uniqueId
-    };
-    this.setState(curState => ({
-      todos: [...curState.todos, newTodo]
-    }));
-  }
+    createNewTodo(todoObj) {
+        let uniqueId = uuid();
+        let newTodo = {
+            ...todoObj,
+            id: uniqueId,
+            key: uniqueId
+        };
+        this.setState(curState => ({
+            todos: [...curState.todos, newTodo]
+        }));
+    }
 
-  editTodo(id) {
-    console.log("edit");
-    console.log(id);
-  }
+    editTodo(id) {
+        console.log("edit");
+        console.log(id);
+    }
 
-  removeTodo(id) {
-    let filteredState = this.state.todos.filter(todo => todo.id !== id);
-    this.setState({
-      todos: [...filteredState]
-    });
-  }
+    removeTodo(id) {
+        let filteredState = this.state.todos.filter(todo => todo.id !== id);
+        this.setState({
+            todos: [...filteredState]
+        });
+    }
 
-  render() {
-    return (
-      <div>
-        <h1>Todo List</h1>
-        {this.state.todos.map(todo => (
-          <Todo
-            text={todo.text}
-            id={todo.id}
-            key={todo.id}
-            removeTodo={this.removeTodo}
-            editTodo={this.editTodo}
-          />
-        ))}
-        <NewTodoForm createNewTodo={this.createNewTodo} uuid={uuid()} />
-      </div>
-    );
-  }
+    updateText(id, newText) {
+        const newState = this.state.todos.map(todo => {
+            if (todo.id === id) {
+                return { ...todo, text: newText };
+            } else {
+                return todo;
+            }
+        });
+        this.setState({ todos: newState });
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Todo List</h1>
+                {this.state.todos.map(todo => (
+                    <Todo
+                        text={todo.text}
+                        id={todo.id}
+                        key={todo.id}
+                        removeTodo={this.removeTodo}
+                        editTodo={this.updateText}
+                    />
+                ))}
+                <NewTodoForm createNewTodo={this.createNewTodo} uuid={uuid()} />
+            </div>
+        );
+    }
 }
 
 export default TodoList;
